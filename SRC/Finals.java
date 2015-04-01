ed6e49dae3b4f476a240fa9c65aac1c75d0c20b1
nicolas <com.montfleury@free.fr>
2015-04-01 22:52:28 +0200

    mise à jour des constantes

--- SRC/Finals.java
+++ SRC/Finals.java
@@ -1,8 +1,27 @@
 public interface Finals {
     /**
-     * Taille du coté d’une unité simple.
+     * Taille du coté d’une unité simple en metres.
      */
-    public final static int SIDE = 2;
-
-
+    public final static int SIDE = 10;
+    
+    /**
+     * Angle de Balayage en degrés.
+     */
+    public final static int ALPHA = 4;
+    
+    /**
+     * Quantité de "vie" d’une unité simple en secondes d’attaque.
+     */
+    public final static int LIFE = 3;
+    
+    /**
+     * Taille de la carte en metres.
+     */
+    public final static int WIDTH = 2000, HEIGTH = 1000;
+    
+    /**
+     * Echelle (distance en metre*scale = distance en pixel).
+     */
+    public static int scale = 5;
+    
 }
