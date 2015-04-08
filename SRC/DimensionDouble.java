27cf43089a1cc2f9c47436c5aceff9901f1bd964
BuonOmo <buonomo.ulysse@gmail.com>
2015-04-08 15:40:26 +0200

    Ajout de la classe permettant de gerer les dimensions en double

---
+++ SRC/DimensionDouble.java
@@ -0,0 +1,8 @@
+import java.awt.Dimension;
+
+public class DimensionDouble extends Dimension {
+    Dimension dim;
+    DimensionDouble(double width, double height){
+        dim.setSize(width, height);
+    }
+}
