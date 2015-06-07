ea8e28b97a1626e70c3e0b64317627e643b2d3d0
ThanatosCG <charles@gaydon.fr>
2015-06-07 16:09:36 +0200

    Boolean et correction de spawnISPossible

--- SRC/Building.java
+++ SRC/Building.java
@@ -10,6 +10,7 @@ public class Building extends Item {
 
     public static LinkedList<Building> buildings = new LinkedList<Building>();
     private int creationTimer;
+    boolean spawnPossible;
     
     /**
      * temps de crÃ©ation, varie en fonction de la taille du batiment.
@@ -78,12 +79,16 @@ public class Building extends Item {
      */
     public boolean spawnIsPossible(){
     	boolean possible = true;
-    	double largeurCarreSpawnNecessaire = 2*Finals.SIDE; // TODO A changer selon taille de l'animation?
+    	double largeurCarreSpawnNecessaire = 4*Finals.SIDE; // TODO A changer selon taille de l'animation?
     	double xS = this.getCenter().getX() - Finals.SIDE/2.0;
     	double yS = this.hitbox.getMaxY() + 1;
-    	Rectangle2D frame = new Rectangle2D.Double( xS, yS, largeurCarreSpawnNecessaire, largeurCarreSpawnNecessaire);
+    	Rectangle2D frame = new Rectangle2D.Double( xS-largeurCarreSpawnNecessaire/2, yS, largeurCarreSpawnNecessaire, largeurCarreSpawnNecessaire);
     	
-        LinkedList<Item> u = getItemInFrame(frame);
+        LinkedList<Item> u = getItemInFrame(frame); //prend en compte le centre des items seulement: on scan sur une large etendue qu'on réduit ensuite
+        frame.setRect(new Rectangle2D.Double( xS-1, yS, Finals.SIDE+2, Finals.SIDE+2));
+        for(int i=0;i<u.size();i++){
+        	if(!u.get(i).hitbox.intersects(frame)){ u.remove(u.get(i));}
+        }
     	if(u.isEmpty()){
     		return possible;
     	}else{
@@ -140,6 +145,7 @@ public class Building extends Item {
             //((UI.time-8) % (int) (4 / (hitbox.getHeight() * UNIT_PER_SECOND) + 1) == 0)
             
         }
+        this.spawnPossible = spawnIsPossible();
         return false;
     }
     
