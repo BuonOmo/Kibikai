191260aa41ab901991584db0fb38ef84f5f416c5
louhammel <lou.hammel@gmail.com>
2015-06-08 17:05:30 +0200

    Avancee des options, actions liees au choix

--- SRC/OptionsFrame.java
+++ SRC/OptionsFrame.java
@@ -8,12 +8,14 @@ import java.io.InputStreamReader;
 import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
+@SuppressWarnings("serial")
 public class OptionsFrame extends JFrame {
 
 	JFrame optFrame;
@@ -25,6 +27,8 @@ public class OptionsFrame extends JFrame {
 	private JLabel colorIALabel;
 	private JComboBox colorPlayerChoice;
 	private JComboBox colorIAChoice;
+	private JCheckBox music;
+	private JCheckBox fog;
 	
 	public static String colorPlayer;
 	public static String colorIA;
@@ -68,6 +72,8 @@ public class OptionsFrame extends JFrame {
     	colorIAChoice.setBounds(optFrameWidth *7/12 - size.width /2, optFrameHeight * 6/12 - size.height /2, size.width, size.height);
     	colorPlayerLabel.setBounds(optFrameWidth *5/12 - size.width /2, optFrameHeight * 4/12 - size.height /2, size.width, size.height);
     	colorIALabel.setBounds(optFrameWidth *5/12 - size.width /2, optFrameHeight * 6/12 - size.height /2, size.width, size.height);
+    	saveChanges.setBounds(optFrameWidth *5/12 - size.width /2, optFrameHeight * 10/12 - size.height /2, 150, size.height);
+    	cancel.setBounds(optFrameWidth *7/12 - size.width /2, optFrameHeight * 10/12 - size.height /2, 150, size.height);
     	
     	
     	//Adding the components to the frame
@@ -75,22 +81,42 @@ public class OptionsFrame extends JFrame {
     	panel.add(colorIAChoice);
     	panel.add(colorPlayerLabel);
     	panel.add(colorIALabel);
+    	panel.add(saveChanges);
+    	panel.add(cancel);
     	optFrame.add(panel);
     	
     	
     	//Action Listeners of the components
     	ActionListener colorPlayerListener = new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                
+                colorPlayer = (String) colorPlayerChoice.getSelectedItem();
             }
         };
         
     	ActionListener colorIAListener = new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                
+                colorIA = (String) colorIAChoice.getSelectedItem();
             }
         };
-    	
+        
+        ActionListener savingChanges = new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		//setOptions in the file
+        	}
+        };
+        
+        ActionListener cancelling = new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		//reinitialize options
+        	}
+        };
+        
+        colorPlayerChoice.addActionListener(colorPlayerListener);
+        colorIAChoice.addActionListener(colorIAListener);
+        saveChanges.addActionListener(savingChanges);
+        cancel.addActionListener(cancelling);
+        
+        
     	//JFrame properties
     	optFrame.setTitle("OPTIONS");
     	optFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -98,14 +124,5 @@ public class OptionsFrame extends JFrame {
     	
     	
 	}
-	
-	public static String getColorPlayer(){
-		return colorPlayer;
-	}
-	
-	public static void setColorPlayer(String color){
-		colorPlayer=color;
-	}
-	
 
 }
